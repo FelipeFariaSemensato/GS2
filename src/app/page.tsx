8eee0abe8591e13d56fae6202fbbd6383d98f2bb
Felipe Faria Semensato <felipef.semensato@gmail.com>
2024-06-03 09:34:41 -0300

    página principal

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,17 @@
-import Image from "next/image";
-
+import './style.css';
 export default function Home() {
   return (
-    <main>
-      
-    </main>
+      <div className="water-consumption">
+      <div className="water-drop">
+        <img src="/washing.png" width={900}/>
+      </div>
+      <div className='text'>
+      <h1>Consumo de Água</h1>
+      <p>Em média, cada pessoa consome cerca de 110 litros de água por dia.</p>
+      <p>Esse uso inclui atividades como banho, escovar os dentes, lavar louça, cozinhar e beber.</p>
+      <p>A conscientização sobre o uso responsável da água é crucial para a preservação desse recurso essencial.</p>
+      <p>Acesse nossa ferramenta pelo cabeçalho clicando na gota e receba sua média de gasto por dia e dicas para melhorar.</p>
+      </div>
+    </div>
   );
 }
